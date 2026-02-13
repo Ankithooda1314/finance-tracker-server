59829c42907e68e19d9c933f2b84aa175487842e
Ankit <ankit@Mac.fritz.box>
2026-02-13 08:13:48 +0100

    render port fix

--- src/index.ts
+++ src/index.ts
@@ -474,7 +474,14 @@ app.delete(
 
 // ================= START =================
 
-app.listen(5000, () => {
-  console.log("Running on port 5000");
+// app.listen(5000, () => {
+//   console.log("Running on port 5000");
+// });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Running on port", PORT);
 });
+
 
